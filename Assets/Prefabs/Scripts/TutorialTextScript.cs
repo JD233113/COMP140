6207e899139472adfe5b783ea2e2d216f9a57319
JD233113 <JD233113@falmouth.ac.uk>
2020-03-04 05:02:43 +0000

    Set up very basic movement tutorial scene

---
+++ Assets/Prefabs/Scripts/TutorialTextScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialTextScript : MonoBehaviour
+{
+    //Destroys the gameobject the script is attached to when a key is pressed
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
