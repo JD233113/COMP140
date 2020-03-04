f0a403facae7b2e0df21d2d853e3a903c277b32b
JD233113 <JD233113@falmouth.ac.uk>
2020-03-04 03:11:21 +0000

    Implemented basic movement, camera follows player

---
+++ Assets/Prefabs/Scripts/Player/CameraScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+
+    //Makes the camera follow the player by moving it to a position based on the player's current position and the specified offset
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, offset.z);
+    }
+}
---
+++ Assets/Prefabs/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float movementScale;
+    public float jumpScale;
+    public float maxSpeed;
+    bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Stores the rigidbody to reduce the number of times GetComponent() needs to be called
+        rb = this.gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        //Checks whether the movement inputs are being used
+        float xMovement = Input.GetAxis("Horizontal");
+
+        //Additional force is only applied if the player is below the maximum speed
+        if (rb.velocity.magnitude < maxSpeed)
+        {
+            Vector2 movement = new Vector2(xMovement, 0);
+
+            //Applies force to the player rigidbody
+            //Direction is based on the input control pressed, acceleration is based on the movementScale variable
+            rb.AddForce(movementScale * movement);
+        }
+
+        //Applies positive force to the rigidbody on the Y axis whenever the jump input is used
+        //Can only jump when grounded
+        if (Input.GetButtonDown("Jump") && isGrounded == true)
+        {
+            Vector2 jumpForce = new Vector2(0, jumpScale);
+            rb.AddForce(jumpForce);
+        }
+    }
+
+    //Marks the player as grounded when in contact with the ground
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "FloorTiles")
+        {
+            isGrounded = true;
+        }
+    }
+
+    //Marks the player as not being grounded when in the air
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "FloorTiles")
+        {
+            isGrounded = false;
+        }
+    }
+}
